f23b7454335b32ea4fadad11aec2d4e8cf0cf6b7
FlutterDev <flutterDev>
2019-10-09 18:12:39 +0300

    setup ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_flutter/Models/TaskModel.dart';
 
-void main() => runApp(MyApp());
+import 'Views/TaskDeteils.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(App());
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To Do List',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'To Do List'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +28,87 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<TaskModel> _tasksList = [];
 
-  void _incrementCounter() {
+  void _addToDoItem(TaskModel task) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _tasksList.add(task);
     });
+  }
+
+  void _changeTaskStatus(int index) {
+    setState(() {
+      _tasksList[index].isComplete = !_tasksList[index].isComplete;
+    });
+  }
+
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _tasksList.length) {
+          return _buildTodoItem(context, _tasksList[index], index);
+        }
+      },
+    );
+  }
+
+  Widget _buildTodoItem(BuildContext context, TaskModel task, int index) {
+    return new Dismissible(
+        key: Key(task.title),
+        onDismissed: (directody) {
+          setState(() {
+            _tasksList.removeAt(index);
+          });
+          Scaffold.of(context)
+              .showSnackBar(SnackBar(content: Text("$task.title dismissed")));
+        },
+        background: Container(color: Colors.red),
+        child: new ListTile(
+          title: new Text(task.title),
+          trailing: new IconButton(
+            icon: _tasksList[index].isComplete
+                ? Icon(Icons.check_box)
+                : Icon(Icons.indeterminate_check_box),
+            color: _tasksList[index].isComplete ? Colors.green : Colors.red,
+            onPressed: () {
+              _changeTaskStatus(index);
+            },
+          ),
+          onTap: () => _pushAddTodoScreen(index: index),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      appBar: AppBar(title: Text(widget.title), actions: <Widget>[
+        IconButton(
+            icon: Icon(
+              Icons.add,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            onPressed: () {
+              setState(() {
+                _pushAddTodoScreen(isNewTask: true);
+              });
+            })
+      ]),
+      body: _buildTodoList(),
     );
+  }
+
+  _pushAddTodoScreen({bool isNewTask = false, int index}) async {
+    final result = await Navigator.of(context).push(new MaterialPageRoute(
+        builder: (context) => TaskDetailScreen(
+              task: (!isNewTask && _tasksList[index] != null)
+                  ? _tasksList[index]
+                  : TaskModel(),
+            )));
+
+    if (result != null && (_tasksList.length == 0 || index == null)) {
+      _tasksList.add(result);
+    }
+    if (result != null && _tasksList[index] != null) {
+      _tasksList[index] = result;
+    }
   }
 }
